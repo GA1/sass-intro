59f79e0f491375ce20f5f3b1d3402939e933a5c9
Kamil Nguyen Van <nvkga1@gmail.com>
2018-07-13 11:02:25 +0200

    Add simple structs

---
+++ data-structs/src/data-structs.js
@@ -0,0 +1,14 @@
+
+var map = new Map()
+map.set(1, 'a')
+map.set(2, 'b')
+
+
+function f() {
+    console.log(map.keys())
+    console.log(map.values())
+    return map.get(1)
+}
+
+
+module.exports = f;
---
+++ data-structs/src/test/test.js
@@ -0,0 +1,5 @@
+const f = require('../data-structs');
+
+test('something', () => {
+  expect(f()).toBe('a');
+});
